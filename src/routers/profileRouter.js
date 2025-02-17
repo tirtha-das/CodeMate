9cb8fda9e0f5728edf4dac049b8636af44b87825
tirtha-das <tdas86577@gmail.com>
2025-02-17 21:01:57 +0530

    build user/details profile

--- src/routers/profileRouter.js
+++ src/routers/profileRouter.js
@@ -2,7 +2,8 @@ const express = require("express");
 const { User } = require("../models/userModel");
 const {userAuth} = require("../middlewares/userAuth")
 const {updateProfileValidation} = require("../validation/updateProfile")
-const {USER_SAFE_DATA} = require("../config/constant")
+const {USER_SAFE_DATA} = require("../config/constant");
+const { Connections } = require("../models/connectionModel");
 
 
 const profileRouter = express.Router();
@@ -30,6 +31,41 @@ profileRouter.get("/profile/view",userAuth,async(req,res)=>{
     }
     
 })
+
+profileRouter.get("/profile/friend/details/:toUserId",userAuth,async(req,res)=>{
+    try{
+         const {loggedInUser} = req.body;
+         const {toUserId} = req.params;
+         //console.log(loggedInUser._id +" "+toUserId);
+         
+         const data = await Connections.find({
+             $or:[{fromUserId:loggedInUser._id,toUserId,status:'accepted'},
+                   {fromUserId:toUserId,toUserId:loggedInUser._id,status:'accepted'}
+             ]
+         }).populate({path:"fromUserId",
+            select:USER_SAFE_DATA
+    }).populate({path:"toUserId",
+        select:USER_SAFE_DATA
+})
+         const userData = data.map((user)=>{
+            if(user.fromUserId===loggedInUser._id){
+                return user.toUserId;
+            }else{
+                return user.fromUserId;
+            }
+
+         })
+ 
+         res.json({
+             message:"profile data succesfully fetched",
+             data:userData
+         })
+ 
+     }catch(err){
+         res.status(400).send(err.message);
+     }
+     
+ })
 
 profileRouter.patch("/profile/update",userAuth,async(req,res)=>{
     try{
